becc02f81d11d34e4e56b0dcd119af51930b345c
Zoranildo <zoranildo22@gmail.com>
2019-08-03 15:14:17 -0300

    twenty-third day

--- src/context.js
+++ src/context.js
@@ -22,11 +22,20 @@ class ProductProvider extends Component {
             return {products: tempProducts};
         })
     }
-    handleDetail = () => {
-        console.log('Hello from detail');
+
+    getItem = id => {
+        const product = this.state.products.find(item => item.id === id)
+        return product
     }
-    addToCart = () => {
-        console.log('Hello from add to cart');
+
+    handleDetail = id => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return {detailProduct:product}
+        })
+    }
+    addToCart = id => {
+        console.log(`Hello from add to cart.id is ${id}`);
     };
     render() {
         return(
